d419917dca5b4d2aa9a8e9fc8cbc8a378ab34b0e
dgd03146 <ibory1220@naver.com>
2024-11-13 20:58:13 +0900

    geojung - responsive image

--- EJ/20240110/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { Beverage, Espresso, Mocha, Whip } from "./BeverageRefactoring";
-
-const espresso: Beverage = new Espresso();
-console.log(espresso.getDescription() + "$" + espresso.cost());
-
-const espressoWithMocha: Mocha = new Mocha(espresso);
-const espressoWithMochaDouble: Mocha = new Mocha(espressoWithMocha);
-const espressoWithMochaDoubleAndWhip: Whip = new Whip(espressoWithMochaDouble);
-console.log(espressoWithMochaDoubleAndWhip.getDescription + "$" + espressoWithMochaDoubleAndWhip.cost());
